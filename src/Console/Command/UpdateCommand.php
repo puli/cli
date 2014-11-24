4b74966a393d00219971132574e4bcac15bcef8e
Bernhard Schussek <bschussek@gmail.com>
2014-11-24 19:10:07 +0100

    Updated CLI to use the package manager

--- src/Console/Command/UpdateCommand.php
+++ src/Console/Command/UpdateCommand.php
@@ -11,6 +11,7 @@
 
 namespace Puli\Cli\Console\Command;
 
+use Puli\PackageManager\ManagerFactory;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -31,6 +32,9 @@ class UpdateCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('Updating');
+        $environment = ManagerFactory::createProjectEnvironment(getcwd());
+        $manager = ManagerFactory::createPackageManager($environment);
+
+        $manager->generateResourceRepository();
     }
 }

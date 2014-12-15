bf0bf5a891413d66442a6e164df4a3bcb022acd8
Bernhard Schussek <bschussek@gmail.com>
2014-12-15 09:18:43 +0100

    Added basic integration test

---
+++ tests/PuliBinTest.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the puli/cli package.
+ *
+ * (c) Bernhard Schussek <bschussek@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Webmozart\tests;
+
+use Symfony\Component\Process\Process;
+
+/**
+ * @since  1.0
+ * @author Bernhard Schussek <bschussek@gmail.com>
+ */
+class PuliBinTest extends \PHPUnit_Framework_TestCase
+{
+    public function testRunHelp()
+    {
+        $rootDir = realpath(__DIR__.'/..');
+        $process = new Process($rootDir.'/bin/puli');
+
+        $status = $process->run();
+
+        $this->assertSame(0, $status);
+        $this->assertStringStartsWith('Puli version ', $process->getOutput());
+    }
+}

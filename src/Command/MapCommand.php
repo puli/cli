2cf48ad0c4acd6b38fda209e6f974ca1ddf46c96
Bernhard Schussek <bschussek@gmail.com>
2014-12-30 10:52:02 +0100

    MapCommand can now filter by package

--- src/Command/MapCommand.php
+++ src/Command/MapCommand.php
@@ -12,6 +12,7 @@
 namespace Puli\Cli\Command;
 
 use Puli\RepositoryManager\ManagerFactory;
+use Puli\RepositoryManager\Package\PackageManager;
 use Puli\RepositoryManager\Repository\RepositoryManager;
 use Puli\RepositoryManager\Repository\ResourceMapping;
 use Symfony\Component\Console\Helper\Table;
@@ -19,6 +20,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Webmozart\Console\Command\Command;
+use Webmozart\Console\Input\InputOption;
 
 /**
  * @since  1.0
@@ -33,32 +35,43 @@ class MapCommand extends Command
             ->setDescription('Show and manipulate resource mappings.')
             ->addArgument('repository-path', InputArgument::OPTIONAL)
             ->addArgument('path', InputArgument::OPTIONAL | InputArgument::IS_ARRAY)
+            ->addOption('root', null, InputOption::VALUE_NONE, 'Show mappings of the root package')
+            ->addOption('package', 'p', InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'Show mappings of a package', null, 'package')
+            ->addOption('all', 'a', InputOption::VALUE_NONE, 'Show mappings of all packages')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $environment = ManagerFactory::createProjectEnvironment(getcwd());
-        $manager = ManagerFactory::createRepositoryManager($environment);
+        $packageManager = ManagerFactory::createPackageManager($environment);
+        $repoManager = ManagerFactory::createRepositoryManager($environment, $packageManager);
 
         if ($input->getArgument('repository-path')) {
-            return $this->addResourceMapping($input, $manager);
+            return $this->mapResource(
+                $input->getArgument('repository-path'),
+                $input->getArgument('path'),
+                $repoManager
+            );
         }
 
-        return $this->listResourceMappings($output, $manager);
+        $packageNames = $this->getPackageNames($input, $packageManager);
+
+        return $this->listResourceMappings($output, $repoManager, $packageNames);
     }
 
     /**
-     * @param InputInterface $input
-     * @param RepositoryManager $manager
+     * @param string            $repositoryPath
+     * @param string[]          $filesystemPaths
+     * @param RepositoryManager $repoManager
      *
      * @return int
      */
-    protected function addResourceMapping(InputInterface $input, RepositoryManager $manager)
+    private function mapResource($repositoryPath, array $filesystemPaths, RepositoryManager $repoManager)
     {
-        $manager->addResourceMapping(new ResourceMapping(
-            $input->getArgument('repository-path'),
-            $input->getArgument('path')
+        $repoManager->addResourceMapping(new ResourceMapping(
+            $repositoryPath,
+            $filesystemPaths
         ));
 
         return 0;
@@ -66,25 +79,78 @@ class MapCommand extends Command
 
     /**
      * @param OutputInterface $output
-     * @param RepositoryManager $manager
+     * @param RepositoryManager $repoManager
      *
      * @return int
      */
-    protected function listResourceMappings(OutputInterface $output, RepositoryManager $manager)
+    private function listResourceMappings(OutputInterface $output, RepositoryManager $repoManager, $packageNames = null)
+    {
+        if (1 === count($packageNames)) {
+            $mappings = $repoManager->getResourceMappings(reset($packageNames));
+            $this->printMappingTable($output, $mappings);
+
+            return 0;
+        }
+
+        foreach ($packageNames as $packageName) {
+            $mappings = $repoManager->getResourceMappings($packageName);
+
+            if (!$mappings) {
+                continue;
+            }
+
+            $output->writeln("<b>$packageName</b>");
+            $this->printMappingTable($output, $mappings);
+            $output->writeln('');
+        }
+
+        return 0;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param PackageManager $packageManager
+     *
+     * @return string[]|null
+     */
+    private function getPackageNames(InputInterface $input, PackageManager $packageManager)
+    {
+        // Display all packages if "all" is set
+        if ($input->getOption('all')) {
+            return $packageManager->getPackages()->getPackageNames();
+        }
+
+        $packageNames = array();
+
+        // Display root if "root" option is given or if no option is set
+        if ($input->getOption('root') || !$input->getOption('package')) {
+            $packageNames[] = $packageManager->getRootPackage()->getName();
+        }
+
+        foreach ($input->getOption('package') as $packageName) {
+            $packageNames[] = $packageName;
+        }
+
+        return $packageNames;
+    }
+
+    /**
+     * @param OutputInterface   $output
+     * @param ResourceMapping[] $mappings
+     */
+    private function printMappingTable(OutputInterface $output, array $mappings)
     {
         $table = new Table($output);
         $table->setStyle('compact');
         $table->getStyle()->setBorderFormat('');
 
-        foreach ($manager->getResourceMappings() as $mapping) {
+        foreach ($mappings as $mapping) {
             $table->addRow(array(
-                '<em>'.$mapping->getRepositoryPath().'</em>:',
-                ' <tt>'.implode('</tt>, <tt>', $mapping->getFilesystemPaths()).'</tt>'
+                '<em>'.$mapping->getRepositoryPath().'</em>',
+                ' '.implode(', ', $mapping->getFilesystemPaths())
             ));
         }
 
         $table->render();
-
-        return 0;
     }
 }

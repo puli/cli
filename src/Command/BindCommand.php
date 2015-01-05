09fd01b0875bc1c66840699e456bc8242dd9d27a
Bernhard Schussek <bschussek@gmail.com>
2015-01-05 21:33:15 +0100

    Added bindings of different states to "puli bind"

--- src/Command/BindCommand.php
+++ src/Command/BindCommand.php
@@ -13,6 +13,7 @@ namespace Puli\Cli\Command;
 
 use Puli\Cli\Util\StringUtil;
 use Puli\RepositoryManager\Discovery\BindingDescriptor;
+use Puli\RepositoryManager\Discovery\BindingState;
 use Puli\RepositoryManager\Discovery\DiscoveryManager;
 use Puli\RepositoryManager\ManagerFactory;
 use Puli\RepositoryManager\Package\PackageManager;
@@ -40,6 +41,10 @@ class BindCommand extends Command
             ->addOption('root', null, InputOption::VALUE_NONE, 'Show bindings of the root package')
             ->addOption('package', 'p', InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'Show bindings of a package', null, 'package')
             ->addOption('all', 'a', InputOption::VALUE_NONE, 'Show bindings of all packages')
+            ->addOption('enabled', null, InputOption::VALUE_NONE, 'Show enabled bindings')
+            ->addOption('disabled', null, InputOption::VALUE_NONE, 'Show disabled bindings')
+            ->addOption('undecided', null, InputOption::VALUE_NONE, 'Show undecided bindings')
+            ->addOption('ignored', null, InputOption::VALUE_NONE, 'Show ignored bindings')
             ->addOption('delete', 'd', InputOption::VALUE_NONE, 'Delete a binding')
             ->addOption('language', null, InputOption::VALUE_REQUIRED, 'The language of the resource query', 'glob')
             ->addOption('param', null, InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'A binding parameter in the form <param>=<value>')
@@ -52,7 +57,6 @@ class BindCommand extends Command
         $environment = ManagerFactory::createProjectEnvironment(getcwd());
         $packageManager = ManagerFactory::createPackageManager($environment);
         $discoveryManager = ManagerFactory::createDiscoveryManager($environment, $packageManager, $logger);
-        $packageNames = $this->getPackageNames($input, $packageManager);
 
         if ($input->getOption('delete')) {
             return $this->removeBinding(
@@ -71,7 +75,10 @@ class BindCommand extends Command
             );
         }
 
-        return $this->listBindings($output, $discoveryManager, $packageNames);
+        $packageNames = $this->getPackageNames($input, $packageManager);
+        $bindingStates = $this->getBindingStates($input, $discoveryManager);
+
+        return $this->listBindings($output, $discoveryManager, $packageNames, $bindingStates);
     }
 
     private function addBinding($query, $language, $typeName, array $parameters, DiscoveryManager $discoveryManager)
@@ -121,25 +128,33 @@ class BindCommand extends Command
      *
      * @return int
      */
-    private function listBindings(OutputInterface $output, DiscoveryManager $discoveryManager, array $packageNames = array())
+    private function listBindings(OutputInterface $output, DiscoveryManager $discoveryManager, array $packageNames, array $bindingStates)
     {
-        if (1 === count($packageNames)) {
-            $bindings = $discoveryManager->getBindings(reset($packageNames));
-            $this->printBindingTable($output, $bindings);
+        $printBindingState = count($bindingStates) > 1;
 
-            return 0;
-        }
+        foreach ($bindingStates as $bindingState) {
+            $bindingStatePrinted = !$printBindingState;
 
-        foreach ($packageNames as $packageName) {
-            $bindings = $discoveryManager->getBindings($packageName);
+            foreach ($packageNames as $packageName) {
+                $bindings = $discoveryManager->getBindings($packageName, $bindingState);
 
-            if (!$bindings) {
-                continue;
+                if (!$bindings) {
+                    continue;
+                }
+
+                if (!$bindingStatePrinted) {
+                    $this->printBindingState($output, $bindingState);
+                    $bindingStatePrinted = true;
+                }
+
+                $styleTag = BindingState::ENABLED === $bindingState ? null : 'fg=red';
+
+                $this->printBindingTable($output, $bindings, $styleTag, $printBindingState);
+
+                if ($printBindingState) {
+                    $output->writeln('');
+                }
             }
-
-            $output->writeln("<b>$packageName</b>");
-            $this->printBindingTable($output, $bindings);
-            $output->writeln('');
         }
 
         return 0;
@@ -172,15 +187,50 @@ class BindCommand extends Command
         return $packageNames;
     }
 
+    private function getBindingStates(InputInterface $input)
+    {
+        $states = array();
+
+        if ($input->getOption('enabled')) {
+            $states[] = BindingState::ENABLED;
+        }
+
+        if ($input->getOption('disabled')) {
+            $states[] = BindingState::DISABLED;
+        }
+
+        if ($input->getOption('undecided')) {
+            $states[] = BindingState::UNDECIDED;
+        }
+
+        if ($input->getOption('ignored')) {
+            $states[] = BindingState::TYPE_NOT_LOADED;
+            $states[] = BindingState::DUPLICATE_TYPE_DEFINITION;
+        }
+
+        return $states ?: array(
+            BindingState::ENABLED,
+            BindingState::DISABLED,
+            BindingState::UNDECIDED,
+            BindingState::TYPE_NOT_LOADED,
+            BindingState::DUPLICATE_TYPE_DEFINITION,
+        );
+    }
+
     /**
      * @param OutputInterface     $output
      * @param BindingDescriptor[] $bindings
      */
-    private function printBindingTable(OutputInterface $output, array $bindings)
+    private function printBindingTable(OutputInterface $output, array $bindings, $styleTag = null, $indent = false)
     {
         $table = new Table($output);
         $table->setStyle('compact');
         $table->getStyle()->setBorderFormat('');
+
+        $paramTag = $styleTag ?: 'comment';
+        $uuidTag = $styleTag ?: 'comment';
+        $queryTag = $styleTag ?: 'em';
+        $typeTag = $styleTag ?: 'tt';
 
         foreach ($bindings as $binding) {
             $parameters = array();
@@ -189,19 +239,55 @@ class BindCommand extends Command
                 $parameters[] = $parameter.'='.StringUtil::formatValue($value);
             }
 
-            $paramString = $parameters
-                ? ' <comment>('.implode(', ', $parameters).')</comment>'
-                : '';
-
             $uuid = substr($binding->getUuid(), 0, 6);
+            $paramString = '('.implode(', ', $parameters).')';
 
-            $table->addRow(array(
-                '<comment>'.$uuid.'</comment> '.
-                '<em>'.$binding->getQuery().'</em>',
-                ' <tt>'.$binding->getTypeName().'</tt>'.$paramString
-            ));
+            if ($indent) {
+                $paramString = $paramString ? " <$paramTag>$paramString</$paramTag>" : '';
+
+                $table->addRow(array(
+                    "    <$uuidTag>".$uuid."</$uuidTag> ".
+                    "<$queryTag>".$binding->getQuery()."</$queryTag>",
+                    " <$typeTag>".$binding->getTypeName()."</$typeTag>".$paramString
+                ));
+            } else {
+                $paramString = $paramString ? " $paramString" : '';
+
+                $table->addRow(array(
+                    "$uuid {$binding->getQuery()}",
+                    " {$binding->getTypeName()}$paramString"
+                ));
+            }
         }
 
         $table->render();
+    }
+
+    private function printBindingState(OutputInterface $output, $bindingState)
+    {
+        switch ($bindingState) {
+            case BindingState::ENABLED:
+                $output->writeln('Enabled bindings:');
+                $output->writeln('');
+                return;
+            case BindingState::DISABLED:
+                $output->writeln('Disabled bindings:');
+                $output->writeln(' (use "puli bind --enable <uuid>" to enable)');
+                $output->writeln('');
+                return;
+            case BindingState::UNDECIDED:
+                $output->writeln('Bindings that are neither enabled nor disabled:');
+                $output->writeln(' (use "puli bind --enable <uuid>" to enable)');
+                $output->writeln('');
+                return;
+            case BindingState::TYPE_NOT_LOADED:
+                $output->writeln('The following bindings are ignored, because their type is not available:');
+                $output->writeln('');
+                return;
+            case BindingState::DUPLICATE_TYPE_DEFINITION:
+                $output->writeln('The following bindings are ignored, because their type is defined twice:');
+                $output->writeln('');
+                return;
+        }
     }
 }

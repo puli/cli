8579dd58449338ad5f2a1fb503e4c0d80b8d9bf3
Bernhard Schussek <bschussek@gmail.com>
2016-01-06 12:11:57 +0100

    Decoupled from classes in the Tests namespaces of other repositories

---
+++ tests/Fixtures/TestDirectory.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+ * This file is part of the puli/cli package.
+ *
+ * (c) Bernhard Schussek <bschussek@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Puli\Cli\Tests\Fixtures;
+
+use Puli\Repository\Api\Resource\PuliResource;
+use Puli\Repository\Resource\Collection\ArrayResourceCollection;
+use Puli\Repository\Resource\GenericResource;
+
+/**
+ * @author Bernhard Schussek <bschussek@gmail.com>
+ */
+class TestDirectory extends GenericResource
+{
+    /**
+     * @var PuliResource[]
+     */
+    private $children = array();
+
+    private $metadata;
+
+    public function __construct($path = null, array $children = array())
+    {
+        parent::__construct($path);
+
+        foreach ($children as $child) {
+            $this->children[$child->getName()] = $child;
+        }
+
+        $this->metadata = new TestMetadata();
+    }
+
+    public function getChild($relPath)
+    {
+        return $this->children[$relPath];
+    }
+
+    public function hasChild($relPath)
+    {
+        return isset($this->children[$relPath]);
+    }
+
+    public function hasChildren()
+    {
+        return count($this->children) > 0;
+    }
+
+    public function listChildren()
+    {
+        return new ArrayResourceCollection($this->children);
+    }
+
+    public function getMetadata()
+    {
+        return $this->metadata;
+    }
+}
